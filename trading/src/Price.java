319ce0c4b7223a93f16b183808c7f9c12d26970f
Addo Wondo <awondo@cse.unsw.edu.au>
2015-03-11 10:08:10 +1100

    Added some attributes and getters to the Price class.

--- trading/src/Price.java
+++ trading/src/Price.java
@@ -2,5 +2,19 @@
  * This class represents the price, parsed from the input file.
  */
 public class Price {
-    private String companyName;
+    private String companyName; // The company name, identical to RIC from the input file. 
+    private double value; // The value of the share. 
+
+    public Price(String companyName, double value){
+        this.companyName = companyName;
+        this.value = value;
+    }
+
+    public String getCompanyName(){
+        return this.companyName;
+    }
+
+    public double getValue(){
+        return this.value;
+    }
 }

704baf278095f69e3eca0069c6c7cf5aad48ff7d
Addo Wondo <awondo@cse.unsw.edu.au>
2015-03-19 08:29:39 +1100

    Resoliving merge conflicts before pull request.

--- trading/src/main/java/OrderManager.java
+++ trading/src/main/java/OrderManager.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;

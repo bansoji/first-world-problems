2201a3a5f589fc3e555e6021abcee885a5eb3c19
blank2 <ajlim2005@hotmail.com>
2015-03-24 02:32:33 +1100

    Updated PriceHistory and TransactionReader. Made adjustments from code reviews.

--- common/src/main/java/PriceHistory.java
+++ common/src/main/java/PriceHistory.java
@@ -1,34 +1,54 @@
 import java.util.*;
 
 /**
- * This class records the price history for all companies
+ * This class records the price history for all companies.
  */
 public class PriceHistory {
-    private HashMap<String, ArrayList> allPrices;
+    private HashMap<String, List<Price>> allPrices;
 
     public PriceHistory(){
-        Map<String, ArrayList> allPrices = new HashMap<String, ArrayList>();
+        this.allPrices = new HashMap<String, List<Price>>();
     }
 
 /*  Given a hashmap company key, this method appends a price to the end of its
-*   corresponding ArrayList value
+*   corresponding ArrayList value.
 */
     public void pushPrice(String company, Price price){
+        if (allPrices.containsKey(company)){
+            List current = allPrices.get(company);
+            current.add(price);
+        } else {
+            List current = new ArrayList<Price>();
+            current.add(price);
+            allPrices.put(company, current);
+        }
         List current = allPrices.get(company);
         current.add(price);
     }
 
     public List getCompanyHistory(String company){
-        return allPrices.get(company);
+        List result = null;
+        if (allPrices.containsKey(company)){
+            result = allPrices.get(company);
+        }
+        return result;
     }
 
     public void printCompanyHistory(String company){
         List<Price> current = allPrices.get(company);
-        for (Price price: current){
-            String companyName = price.getCompanyName();
-            String companyDate = price.getDate().toString();
-            String companyValue = String.valueOf(price.getValue());
-            System.out.println(companyName + companyDate + companyValue);
+        if (current != null) {
+            for (Price price: current) {
+                String companyName = price.getCompanyName();
+                String companyDate = price.getDate().toString();
+                String companyValue = String.valueOf(price.getValue());
+                System.out.println(companyName + companyDate + companyValue);
+            }
+        } else {
+            System.out.println("There is no company to print.");
         }
+    }
+
+    public HashMap<String, List<Price>> getAllPrices(){
+        return allPrices;
     }
 }
--- common/src/main/java/TransactionReader.java
+++ common/src/main/java/TransactionReader.java
@@ -6,6 +6,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Date;
 import java.util.logging.Logger;
@@ -16,6 +17,7 @@ import java.util.logging.Logger;
 
 public class TransactionReader {
     private CSVReader reader;
+    private PriceHistory allPrices;
 
     //column numbers for input prices data file
     private static final int COMPANY_NAME = 0;
@@ -37,15 +39,12 @@ public class TransactionReader {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        PriceHistory allPrices = new PriceHistory();
     }
 
-    public List<Price> getAllPrices() {
-        List<Price> allPrices = new ArrayList<Price>();
-
-        //Read the remaining lines and store data into ArrayList
+    public void readAllPrices() {
         try {
-            //Skip first line
-            reader.readNext();
+            reader.readNext();     //Skip first line
             String[] nextLine;
             while ((nextLine = reader.readNext()) != null) {
                 String companyName = nextLine[COMPANY_NAME];
@@ -67,55 +66,24 @@ public class TransactionReader {
                 }
 
                 Price newPrice = new Price(companyName, value, date);
-                allPrices.add(newPrice);
+                allPrices.pushPrice(companyName, newPrice);
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return allPrices;
     }
 
-    public List<Order> getAllOrders() {
-        List<Order> allOrders = new ArrayList<Order>();
+    public List getCompanyPrices(String company){
+        List companyHistory = allPrices.getCompanyHistory(company);
+        return companyHistory;
+    }
 
-        //Read the remaining lines and store data into ArrayList
-        try {
-            String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-                String companyName = nextLine[ORDER_COMPANY_NAME];
-                double value;
-                Date date = null;
+    public void printCompanyPrices(String company){
+        allPrices.printCompanyHistory(company);
+    }
 
-                if (nextLine[ORDER_PRICE].equals("")) {
-                    // no value
-                    continue;
-                }
-                value = Double.parseDouble(nextLine[ORDER_PRICE]);
-
-                try {
-                    DateFormat df = new SimpleDateFormat("dd/mm/yyyy hh:mm:ss");
-                    date = df.parse(nextLine[ORDER_DATE]);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-                OrderType type;
-                if (nextLine[ORDER_SIGNAL].equals("B")) {
-                    type = OrderType.BUY;
-                } else {
-                    type = OrderType.SELL;
-                }
-
-                int volume = Integer.parseInt(nextLine[ORDER_VOLUME]);
-
-                Order order = new Order(type,companyName, value, volume, date);
-                allOrders.add(order);
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return allOrders;
+    public HashMap<String, ArrayList> getAllPrices(){
+        return allPrices.getAllPrices();
     }
 }

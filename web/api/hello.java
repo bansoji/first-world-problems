441714eaa65fd6da2603192d03808017a4ecded3
Addo Wondo <awondo@cse.unsw.EDU.AU>
2015-03-23 21:41:35 +1100

    Basics

---
+++ web/api/hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class hello
+{
+    public static void main( String args[] )
+    {
+        System.out.println("Content-Type: text/html\n");
+        System.out.println("");
+        System.out.println("<html>");
+
+        System.out.println("Hello World");
+    }
+}

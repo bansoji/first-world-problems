eb13740353005b08f784ae65d82117ae4eaf7966
Addo Wondo <awondo@cse.unsw.edu.au>
2015-03-28 22:54:52 +1100

    Added start and end dates into the parameter file.
    And breifly tested.

--- trading/src/main/java/MomentumStrategy.java
+++ trading/src/main/java/MomentumStrategy.java
@@ -2,7 +2,11 @@ import com.sun.javafx.tools.packager.Log;
 import finance.FinanceUtils;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 import java.util.Properties;
@@ -17,6 +21,9 @@ public class MomentumStrategy implements TradingStrategy {
     private int movingAverage;
     private double threshold;
     private int volume;
+    private Date startDate;
+    private Date endDate;
+
 
     private static final Logger logger = Logger.getLogger("log");
 
@@ -33,11 +40,26 @@ public class MomentumStrategy implements TradingStrategy {
             e.printStackTrace();
         }
 
+        DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
         // Configure the strategy using parameters config properties file.
         // Defaults are the same as in MSM spec.
         this.movingAverage = Integer.parseInt(prop.getProperty("movingAverage", "4"));
         this.threshold = Double.parseDouble(prop.getProperty("threshold", "0.001"));
         this.volume = Integer.parseInt(prop.getProperty("volume", "100"));
+
+        System.out.println(prop.getProperty("startDate"));
+        try {
+            this.startDate = df.parse(prop.getProperty("startDate"));
+        } catch (ParseException e) {
+            logger.warning("Incorrect Date format used for Start Date of simulations.");
+            startDate = null;
+        }
+        try {
+            this.endDate = df.parse(prop.getProperty("endDate"));
+        } catch (ParseException e) {
+            logger.warning("Incorrect Date format used for End Date of simulations.");
+            endDate = null;
+        }
 
         logger.info("Parameters Found:\n" +
                 "Moving Average: " + this.movingAverage + "\n" +
@@ -80,6 +102,12 @@ public class MomentumStrategy implements TradingStrategy {
                 // Create an order using this ith day.
                 // Get the price for that day. Offset by moving average.
                 Price tradePrice = prices.get(i + movingAverage -1);
+
+                // Skip if the date given is out of the simulation date range.
+                if (startDate != null && tradePrice.getDate().before(startDate)) continue;
+                if (endDate != null && tradePrice.getDate().after(endDate)) continue;
+
+                // Create a new Order.
                 Order o = new Order(nextStatus, tradePrice.getCompanyName(), tradePrice.getValue(),
                         volume, tradePrice.getDate());
                 //System.out.println("Out");

27565c916f4f79a0f0d5bdb16fce196612bc1320
zippywin <eg264@hotmail.com>
2015-03-30 01:04:06 +1100

    added unit test class for Order

---
+++ trading/src/test/java/OrderTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderTest {
+
+    @Test
+    public void testGetCompanyName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPrice() throws Exception {
+
+    }
+
+    @Test
+    public void testGetValue() throws Exception {
+
+    }
+
+    @Test
+    public void testTotalTransactionValue() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOrderType() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOrderDate() throws Exception {
+
+    }
+
+    @Test
+    public void testToStringArray() throws Exception {
+
+    }
+}

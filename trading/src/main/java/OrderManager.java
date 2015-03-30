d7a04792b87947a065a1a9722a160d1c100556e2
Addo Wondo <awondo@cse.unsw.edu.au>
2015-03-30 16:08:42 +1100

    Added the output file names ot the logger.
    These are defined in the order manager.

--- trading/src/main/java/OrderManager.java
+++ trading/src/main/java/OrderManager.java
@@ -15,6 +15,8 @@ public class OrderManager {
 
     // Some class constants.
     public static final String VERSION = "0.9.0";
+    public static final String OUTPUT_FILE = "orders.csv";
+    public static final String LOG_FILE = "logfile.log";
 
     public static void main(String[] args) throws IOException {
         String fileName = args[0];
@@ -27,7 +29,7 @@ public class OrderManager {
 
         // Logger initialisation.
         Logger logger = Logger.getLogger("log");
-        FileHandler handler = new FileHandler("logfile.log");
+        FileHandler handler = new FileHandler(LOG_FILE);
         SimpleFormatter formatter = new SimpleFormatter();
         handler.setFormatter(formatter);
         logger.addHandler(handler);
@@ -35,7 +37,9 @@ public class OrderManager {
         logger.info("====== Buy Hard =========\n" +
                 "Developer Team: Group 1\n" +
                 "MODULE VERSION: " + VERSION + "\n" +
-                "INPUT FILE: " + fileName);
+                "INPUT FILE: " + fileName + "\n" +
+                "OUTPUT FILE: " + OUTPUT_FILE + "\n" +
+                "LOG FILE: " + LOG_FILE);
 
         // Load the csv file.
         TransactionReader tReader = new TransactionReader(fileName);
@@ -59,7 +63,7 @@ public class OrderManager {
         strategy.generateOrders();
         List<Order> ordersGenerated = strategy.getOrders();
 
-        FileWriter file = new FileWriter("output.csv");
+        FileWriter file = new FileWriter(OUTPUT_FILE);
         Printer.printOrders(ordersGenerated, file);
         file.close();
 

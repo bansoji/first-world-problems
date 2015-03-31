9301e2f8ecd082e889fa137bb1bbed519c2ea0cb
zippywin <eg264@hotmail.com>
2015-03-31 23:05:01 +1100

    Added test data to testGenerateOrders

--- trading/src/test/java/MomentumStrategyTest.java
+++ trading/src/test/java/MomentumStrategyTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,15 +13,17 @@ public class MomentumStrategyTest {
 
     @Test
     public void testGenerateOrders() throws Exception {
-        String paramName = "trading/resources/config.properties";
+        String paramName = "resources/config.properties";
         InputStream input = new FileInputStream(paramName);
-        Price price00 = new Price("CompanyA", 15.00, new Date(0));
-        Price price01 = new Price("CompanyA", 13.25, new Date(1));
-        Price price02 = new Price("CompanyA", 16.00, new Date(2));
-        Price price03 = new Price("CompanyA", 17.35, new Date(3));
-        Price price04 = new Price("CompanyA", 17.35, new Date(4));
-        Price price05 = new Price("CompanyA", 9.01, new Date(5));
-        Price price06 = new Price("CompanyA", 9.00, new Date(6));
+        SimpleDateFormat testDates = new SimpleDateFormat("dd/MM/yyyy");
+
+        Price price00 = new Price("CompanyA", 8.00, testDates.parse("01/01/2013"));
+        Price price01 = new Price("CompanyA", 13.25, testDates.parse("02/01/2013"));
+        Price price02 = new Price("CompanyA", 16.00, testDates.parse("03/01/2013"));
+        Price price03 = new Price("CompanyA", 17.35, testDates.parse("04/01/2013"));
+        Price price04 = new Price("CompanyA", 17.35, testDates.parse("05/01/2013"));
+        Price price05 = new Price("CompanyA", 9.01, testDates.parse("06/01/2013"));
+        Price price06 = new Price("CompanyA", 9.00, testDates.parse("07/01/2013"));
         List<Price> priceList = new ArrayList<>();
         priceList.add(price00);
         priceList.add(price01);
@@ -35,7 +38,10 @@ public class MomentumStrategyTest {
         List<Order> testOutput = testStrategy.getOrders();
         for (Order i : testOutput)
         {
-            System.out.println(i.toStringArray());
+            for (String field : i.toStringArray())
+            {
+                System.out.println(field);
+            }
         }
 
 

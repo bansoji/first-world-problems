810de0cacdffef04500276931c4922260bec2c83
zippywin <eg264@hotmail.com>
2015-05-08 00:47:32 +1000

    Added prototype createLine to GemoetryUtils, cleaned Channel

---
+++ trading/src/main/java/utils/Point.java
@@ -0,0 +1,22 @@
+package utils;
+
+/**
+ * Created by Edwin Li on 8/05/2015.
+ */
+public class Point {
+    private double time; //The x-coordinate, the date
+    private double price; // The y-coordinate, the price at that date
+
+    public Point (double time, double price) {
+        this.time = time;
+        this.price = price;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}

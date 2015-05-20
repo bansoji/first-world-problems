6c09f73bbd9617e7c9b4da5e76d2449c2b5e431a
Banson <bsmt598@cse.unsw.edu.au>
2015-05-21 02:55:15 +1000

    Added logo to PDF and improved PDF styling

--- ui/src/main/java/ReportGenerator.java
+++ ui/src/main/java/ReportGenerator.java
@@ -9,10 +9,12 @@ import main.Returns;
 import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
 import net.sf.dynamicreports.jasper.builder.export.JasperXlsxExporterBuilder;
 import net.sf.dynamicreports.jasper.constant.JasperProperty;
+import net.sf.dynamicreports.report.base.DRSubtotal;
 import net.sf.dynamicreports.report.builder.DynamicReports;
 import net.sf.dynamicreports.report.builder.column.TextColumnBuilder;
 import net.sf.dynamicreports.report.builder.component.MultiPageListBuilder;
 import net.sf.dynamicreports.report.builder.component.TextFieldBuilder;
+import net.sf.dynamicreports.report.builder.style.ConditionalStyleBuilder;
 import net.sf.dynamicreports.report.builder.style.StyleBuilder;
 import net.sf.dynamicreports.report.constant.HorizontalAlignment;
 import net.sf.dynamicreports.report.constant.PageOrientation;
@@ -22,6 +24,7 @@ import net.sf.dynamicreports.report.exception.DRException;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 
+import javax.swing.text.Style;
 import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -48,6 +51,9 @@ public class ReportGenerator {
     private StyleBuilder titleStyle;
     private StyleBuilder tableHeader;
     private StyleBuilder tableBody;
+    private StyleBuilder tableBodyBolded;
+    private StyleBuilder positiveVal;
+    private StyleBuilder negativeVal;
     private StyleBuilder boldCentered;
 
     private static String pdfReportName = "report.pdf";
@@ -110,30 +116,35 @@ public class ReportGenerator {
     public void generatePDF()
     {
 
-        // build Styles
+        // build PDF Styles
 
         tableHeader = DynamicReports.stl.style()
                 .bold()
                 .setHorizontalAlignment(HorizontalAlignment.LEFT)
                 .setBorder(DynamicReports.stl.pen1Point())
-                .setBackgroundColor(Color.LIGHT_GRAY)
+                .setBackgroundColor(new Color(222, 222, 222))
                 .setPadding(3);
         tableBody = DynamicReports.stl.style()
                 .setHorizontalAlignment(HorizontalAlignment.LEFT)
                 .setBorder(DynamicReports.stl.penThin())
                 .setPadding(3);
+        tableBodyBolded = DynamicReports.stl.style(tableBody).bold();
         titleStyle = DynamicReports.stl.style()
-                .setFontSize(30)
+                .setFontSize(20)
                 .setBottomPadding(20)
                 .bold();
         subtitleStyle = DynamicReports.stl.style()
-                .setFontSize(20)
-                .setBottomPadding(20)
+                .setFontSize(15)
+                .setBottomPadding(15)
                 .setTopPadding(20)
                 .bold();
         boldCentered = DynamicReports.stl.style()
                 .bold()
                 .setHorizontalAlignment(HorizontalAlignment.CENTER);
+        positiveVal = DynamicReports.stl.style()
+                .setBackgroundColor(new Color(210,255,210));
+        negativeVal = DynamicReports.stl.style()
+                .setBackgroundColor(new Color(255,210,210));
 
 
         JasperReportBuilder totalSummary = buildTotalSummaryCmp();
@@ -146,8 +157,19 @@ public class ReportGenerator {
         report.setPageFormat(PageType.A4, PageOrientation.LANDSCAPE);
         report.setPageColumnsPerPage(2);
         report.setPageColumnSpace(15);
-        TextFieldBuilder<String> title = DynamicReports.cmp.text("BuyHard Report - Overview");
-        report.title(title.setStyle(titleStyle));
+
+        // Main Report Header
+        TextFieldBuilder<String> title = DynamicReports.cmp.text("Report - Overview");
+        report.title(DynamicReports.cmp.horizontalList().add(
+                DynamicReports.cmp.image(getClass().getResource("logosizes/BuyHard2Logo_Small.png"))
+                        .setFixedHeight(40)
+                        .setHorizontalAlignment(HorizontalAlignment.LEFT),
+                title.setStyle(titleStyle)
+                        .setHorizontalAlignment(HorizontalAlignment.RIGHT)
+        ).newRow().add(DynamicReports.cmp.filler().setStyle(
+                        DynamicReports.stl.style().setTopBorder(DynamicReports.stl.penThin())
+                ).setFixedHeight(10)
+        ));
         report.detail(DynamicReports.cmp.multiPageList(
                         DynamicReports.cmp.subreport(totalSummary),
                         DynamicReports.cmp.subreport(assetReport),
@@ -158,8 +180,11 @@ public class ReportGenerator {
 
         report.setDataSource(new JREmptyDataSource());
 
-        // Sum columns.
-        report.pageFooter(DynamicReports.cmp.pageXofY().setStyle(boldCentered)); //shows number of page at page footer
+        // Main Report Footer
+        report.pageFooter(DynamicReports.cmp.verticalList().add(DynamicReports.cmp.filler().setStyle(
+                        DynamicReports.stl.style().setTopBorder(DynamicReports.stl.penThin())
+                ).setFixedHeight(5),
+                DynamicReports.cmp.pageXofY().setStyle(boldCentered)));
 
 
 
@@ -211,6 +236,13 @@ public class ReportGenerator {
     {
         TextColumnBuilder<String> companyNameColumn = DynamicReports.col.column("Company", "company", DynamicReports.type.stringType());
         TextColumnBuilder<Double> equityValueColumn = DynamicReports.col.column("Equity", "equity", DynamicReports.type.doubleType());
+
+        // Conditional Styling
+        ConditionalStyleBuilder positive = DynamicReports.stl.conditionalStyle(DynamicReports.cnd.greater(equityValueColumn,0))
+                .setBackgroundColor(new Color(210,255,210));
+        ConditionalStyleBuilder negative = DynamicReports.stl.conditionalStyle(DynamicReports.cnd.smaller(equityValueColumn, 0))
+                .setBackgroundColor(new Color(255,210,210));
+
         // Asset value
         TextFieldBuilder<String> subtitle = DynamicReports.cmp.text("Equities");
         JRDataSource assetData = createDataSource();
@@ -219,10 +251,14 @@ public class ReportGenerator {
         assetReport.columns(companyNameColumn, equityValueColumn);
         assetReport.setColumnHeaderStyle(tableHeader);
         assetReport.setColumnStyle(tableBody);
+        assetReport.detailRowHighlighters(positive, negative);
         assetReport.setDataSource(assetData);
 
         // Sum of Equities.
-        assetReport.subtotalsAtSummary(DynamicReports.sbt.sum(equityValueColumn));
+        assetReport.subtotalsAtSummary(DynamicReports.sbt.sum(equityValueColumn)
+                .setStyle(tableBodyBolded));
+
+
 
         return assetReport;
     }
@@ -232,6 +268,13 @@ public class ReportGenerator {
         TextColumnBuilder<String> companyNameColumn = DynamicReports.col.column("Company", "company", DynamicReports.type.stringType());
         TextColumnBuilder<Double> returnValueColumn = DynamicReports.col.column("Return", "returnValue", DynamicReports.type.doubleType());
         TextColumnBuilder<Double> returnPercentageColumn = DynamicReports.col.column("Return %", "returnPercentage", DynamicReports.type.doubleType()); //TODO THis should use percentType.
+
+        // Conditional Styling
+        ConditionalStyleBuilder positive = DynamicReports.stl.conditionalStyle(DynamicReports.cnd.greater(returnValueColumn,0))
+                .setBackgroundColor(new Color(210, 255, 210));
+        ConditionalStyleBuilder negative = DynamicReports.stl.conditionalStyle(DynamicReports.cnd.smaller(returnValueColumn, 0))
+                .setBackgroundColor(new Color(255, 210, 210));
+
         // Return value
         TextFieldBuilder<String> subtitle = DynamicReports.cmp.text("Returns");
         JRDataSource resultsData = createDataSource();
@@ -240,10 +283,14 @@ public class ReportGenerator {
         returnsReport.columns(companyNameColumn, returnValueColumn, returnPercentageColumn);
         returnsReport.setColumnHeaderStyle(tableHeader);
         returnsReport.setColumnStyle(tableBody);
+        returnsReport.detailRowHighlighters(positive,negative);
         returnsReport.setDataSource(resultsData);
 
         // Sum of Returns.
-        returnsReport.subtotalsAtSummary(DynamicReports.sbt.sum(returnValueColumn), DynamicReports.sbt.sum(returnPercentageColumn));
+        returnsReport.subtotalsAtSummary(
+                DynamicReports.sbt.sum(returnValueColumn).setStyle(tableBodyBolded),
+                DynamicReports.sbt.sum(returnPercentageColumn).setStyle(tableBodyBolded)
+        );
 
         return returnsReport;
     }

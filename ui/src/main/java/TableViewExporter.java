06783d6a4e4d68da540233a6363ae5646bcc0193
Addo Wondo <awondo@cse.unsw.edu.au>
2015-05-21 17:59:10 +1000

    Started table export.

---
+++ ui/src/main/java/TableViewExporter.java
@@ -0,0 +1,29 @@
+import com.opencsv.CSVWriter;
+
+import javax.swing.text.TableView;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * This class exports a table view.
+ */
+public class TableViewExporter {
+
+    public static void exportCsv(TableView tv, String filename) throws IOException {
+
+        /*
+        CSVWriter writer = new CSVWriter(new FileWriter(filename), ',', CSVWriter.NO_QUOTE_CHARACTER);
+        // feed in your array (or convert your data to an array)
+        String[] entries = "first#second#third".split("#");
+        writer.writeNext(entries);
+        writer.close();
+
+        */
+
+
+
+
+
+    }
+}

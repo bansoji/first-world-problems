958979684385b43475b1258c36cc0c4b50c96353
zippywin <eg264@hotmail.com>
2015-05-23 15:15:10 +1000

    Integration  - added support for Awesome (untested)

---
+++ integration/src/main/java/OrderReaderAwesome.java
@@ -0,0 +1,24 @@
+import main.History;
+import main.Order;
+import main.OrderParser;
+import main.Reader;
+
+/**
+ * This class reads an input CSV file and outputs its contents as an ArrayList.
+ */
+
+public class OrderReaderAwesome extends Reader<Order> {
+
+    public OrderReaderAwesome(String fileName) {
+        parser = new OrderParserAwesome(fileName);
+        history = new History<Order>();
+    }
+
+    protected boolean readNext()
+    {
+        Order nextOrder = (Order)parser.parseNextLine();
+        if (nextOrder == null) return false;
+        history.add(nextOrder.getCompanyName(),nextOrder);
+        return true;
+    }
+}

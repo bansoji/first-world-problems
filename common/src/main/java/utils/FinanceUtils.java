919703533a03888fae7cbfc9b07d53ccc6ae56d0
zippywin <eg264@hotmail.com>
2015-05-28 14:51:50 +1000

    feature - added some code for sharpe ratio calculation

--- common/src/main/java/utils/FinanceUtils.java
+++ common/src/main/java/utils/FinanceUtils.java
@@ -1,5 +1,10 @@
 package utils;
 
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,5 +54,32 @@ public class FinanceUtils {
     public static double calcProfit (double prevPrice, double currPrice, long volume)
     {
         return (currPrice-prevPrice)*volume;
+    }
+
+    /**
+     * Calculates the Sharpe Ratio for a particular strategy's performance on a particular company over a period of time.
+     * @param returns       The list of individual returns given by a strategy over a period of time (for this company).
+     * @param startDate     The starting date the strategy is used.
+     * @param endDate       The ending date the strategy stops at.
+     * @return              The Sharpe Ratio for that time period.
+     */
+    public static double calcSharpeRatio (List<Double> returns, DateTime startDate, DateTime endDate) {
+        // startDate and endDate required to calculate appropriate risk free rate for different time periods other than yearly.
+        int totalNumOfDays = Days.daysBetween(startDate.toLocalDate(), endDate.toLocalDate()).getDays();
+        List<Double> returnPercents = calcAllReturns(returns);
+        double averageReturnPeriod = totalNumOfDays / returnPercents.size();
+        double adjustedRiskFreeRate = Math.pow(1.05, (averageReturnPeriod / 365)) - 1;
+
+        //Calculate necessary statistics
+        DescriptiveStatistics calculator = new DescriptiveStatistics();
+        for (double percent : returnPercents) {
+            calculator.addValue(percent);
+        }
+
+        double effectiveAvgReturn = calculator.getMean() - adjustedRiskFreeRate;
+        double returnDeviation = calculator.getStandardDeviation();
+        double sharpeRatio = effectiveAvgReturn / returnDeviation;
+
+        return sharpeRatio;
     }
 }
